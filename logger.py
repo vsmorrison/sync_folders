972d39a92835b486f495235986e4451497b1eb75
vsmorrison <50921643+vsmorrison@users.noreply.github.com>
2022-01-24 00:57:35 +0300

    Add custom logger creation

---
+++ logger.py
@@ -0,0 +1,17 @@
+import logging
+import sys
+
+
+def make_logger(log_path, time):
+    logger = logging.getLogger("sync_log")
+    logger.setLevel(logging.INFO)
+    log_file = logging.FileHandler(
+        filename=f'{log_path}/Sync report [{time}.log]'
+    )
+    log_format = logging.Formatter('[%(asctime)s] %(levelname)s -- %(message)s')
+    log_file.setFormatter(log_format)
+    logger.addHandler(log_file)
+    terminal_output = logging.StreamHandler(sys.stdout)
+    terminal_output.setFormatter(log_format)
+    logger.addHandler(terminal_output)
+    return logger
